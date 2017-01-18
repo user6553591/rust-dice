4ed21c8206e4fea28a272545d2f098686953eb68
Cooper EdenDay <cooperedenday@protonmail.com>
2017-01-18 09:13:36 -0500

    Added basic dice functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 extern crate rand;
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::os::OsRng::new().unwrap_or_else(|e| {
+        println!("Could not initialize random number generator: {}", e);
+        std::process::exit(1);
+    });
+    println!("{:?}", rng.gen_range(1, 7));
 }
