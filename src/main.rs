0733cf1f218a443bb9f81bdd621ad2d457ff6211
Cooper EdenDay <cooperedenday@protonmail.com>
2017-01-19 00:21:04 -0500

    Get program ready for CLI interaction.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
+    let sides = 6;
     let mut rng = rand::os::OsRng::new().unwrap_or_else(|e| {
         println!("Could not initialize random number generator: {}", e);
         std::process::exit(1);
     });
-    println!("{:?}", rng.gen_range(1, 7));
+    println!("{:?}", rng.gen_range(0, sides) + 1);
 }
