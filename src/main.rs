6583caf6e1e1e3a5e79276e407c8456fa70d12b3
Cooper EdenDay <cooperedenday@protonmail.com>
2017-01-19 06:28:28 -0500

    Added basic error handling for mismatched types.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ extern crate clap;
 use rand::Rng;
 use std::str::FromStr;
 
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let matches = clap::App::new("rust-dice")
+    let matches = clap::App::new(NAME)
         .version(VERSION)
         .author("user6553591 <reddit.com/u/user6553591>")
-        .about("A simple CLI program that simulates dice")
+        .about(DESCRIPTION)
         .arg(clap::Arg::with_name("sides")
             .help("The nuber of sides on the die")
             .takes_value(true)
@@ -19,10 +21,13 @@ fn main() {
             .multiple(false))
         .get_matches();
 
-    let sides = u8::from_str(matches.value_of("sides").unwrap_or("6")).unwrap();
+    let sides = u8::from_str(matches.value_of("sides").unwrap_or("6")).unwrap_or_else(|_| {
+        println!("Type mismatch: Exiting.");
+        std::process::exit(1);
+    });
 
-    let mut rng = rand::os::OsRng::new().unwrap_or_else(|e| {
-        println!("Could not initialize random number generator: {}", e);
+    let mut rng = rand::os::OsRng::new().unwrap_or_else(|_| {
+        println!("Could not initialize random number generator: Exiting.");
         std::process::exit(1);
     });
 
