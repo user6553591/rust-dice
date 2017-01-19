e3f3009d631acf2d9eee8da52335e7ef3a8479da
Cooper EdenDay <cooperedenday@protonmail.com>
2017-01-19 06:34:28 -0500

    Made all types explicit.

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ use rand::Rng;
 use std::str::FromStr;
 
 const NAME: &'static str = env!("CARGO_PKG_NAME");
+const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
 const DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
-    let matches = clap::App::new(NAME)
+    let matches: clap::ArgMatches = clap::App::new(NAME)
         .version(VERSION)
-        .author("user6553591 <reddit.com/u/user6553591>")
+        .author(AUTHORS)
         .about(DESCRIPTION)
         .arg(clap::Arg::with_name("sides")
             .help("The nuber of sides on the die")
@@ -21,12 +22,12 @@ fn main() {
             .multiple(false))
         .get_matches();
 
-    let sides = u8::from_str(matches.value_of("sides").unwrap_or("6")).unwrap_or_else(|_| {
+    let sides: u8 = u8::from_str(matches.value_of("sides").unwrap_or("6")).unwrap_or_else(|_| {
         println!("Type mismatch: Exiting.");
         std::process::exit(1);
     });
 
-    let mut rng = rand::os::OsRng::new().unwrap_or_else(|_| {
+    let mut rng: rand::os::OsRng = rand::os::OsRng::new().unwrap_or_else(|_| {
         println!("Could not initialize random number generator: Exiting.");
         std::process::exit(1);
     });
